a74077736127ea1dd997fdd4365af9ef0d6afc11
dev_T.H <kumi7214@gmail.com>
2022-03-02 17:28:33 +0900

    webpack 세팅전으로 롤백

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,11 @@
-
 const socket = io();
-
 const myFace = document.querySelector('#myFace');
-const muteBtn = document.querySelector("#mute")
-const cameraBtn = document.querySelector('#camera')
-const camerasSelect = document.querySelector('#cameras')
+const muteBtn = document.querySelector("#mute");
+// const cameraBtn = document.querySelector('#camera');
+const camerasSelect = document.querySelector('#cameras');
 // stream 은 비디오와 오디오가 결합된 것
 
-const call = document.querySelector('#call')
+const call = document.querySelector('#call');
 
 call.hidden = true;
 
@@ -15,9 +13,12 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myNickname;
 let myPeerConnection;
+let mySocketID;
+let users = new Map();
 
-async function getAudios() {
+async function getCameras() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const audios = devices.filter(device=> device.kind === 'audioinput');
@@ -28,65 +29,92 @@ async function getAudios() {
             console.log(audio.deviceId)
             console.log(audio.label)
             const option = document.createElement('option');
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getAudios() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const audios = devices.filter(device=> device.kind === 'videoinput');
+        const currentAudio = myStream.getAudioTracks()[0];
+        audios.map(audio => {
+            const option = document.createElement('option');
             option.value = audio.deviceId;
-            option.innerText = audio.label
+            console.log(audio.label)
+            option.innerText = audio.label;
             if (currentAudio.label === audio.label) {
                 option.selected = true;
             }
-            camerasSelect.appendChild(option)
+            camerasSelect.appendChild(option);
         })
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
+
 
 async function getMedia(deviceId) {
     const initialConstrains = {
         audio : true,
-        // video : { facingMode : 'user' },
         video : false
     }
     const cameraConstrains = {
         audio : true,
         video : false
+
     }
     try {
         myStream = await navigator.mediaDevices.getUserMedia(
             deviceId ? cameraConstrains : initialConstrains
-        )
-        myFace.srcObject = myStream
+        );
+        // myFace.srcObject = myStream;
         if (!deviceId) {
             await getAudios();
-        }
+        };
     }
     catch (e) {
-        console.log(e)
+        console.log(e);
     }
 }
 
+
 // 음성 온오프
+
 function handleMuteClick() {
-    // console.log(myStream.getAudioTracks());
-    myStream.getAudioTracks().forEach(track=>track.enabled =!track.enabled);
-    if (!muted) {
-        muteBtn.innerHTML = 'Unmute';
-        muted = true;
-    } else {
-        muteBtn.innerHTML = 'Mute';
-        muted = false;
+    if (myStream) {
+        myStream.getAudioTracks().forEach(track=>track.enabled =!track.enabled);
+        if (!muted) {
+            muteBtn.innerHTML = 'Unmute';
+            muted = true;
+        } else {
+            muteBtn.innerHTML = 'Mute';
+            muted = false;
+        }
     }
 }
+
 // 카메라 온오프
 function handlecameraClick() {
     myStream.getVideoTracks().forEach(track=>track.enabled =!track.enabled);
-    if (cameraOff) {
-        cameraBtn.innerText = "Turn Camera Off";
-        cameraOff = false;
-    } else {
+    // if (cameraOff) {
+        // cameraBtn.innerText = "Turn Camera Off";
+        // cameraOff = false;
+    // } else {
         cameraBtn.innerText = "Turn Camera On";
         cameraOff = true;
-    }
+    // }
 }
+
+
 async function handleCameraChange() {
     // getMedia => 나의 미디어 트랙을 찾는 함수
     await getMedia(camerasSelect.value);
@@ -101,7 +129,7 @@ async function handleCameraChange() {
     }
 }
 muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handlecameraClick);
+// cameraBtn.addEventListener("click", handlecameraClick);
 camerasSelect.addEventListener('input', handleCameraChange);
 
 
@@ -110,53 +138,14 @@ camerasSelect.addEventListener('input', handleCameraChange);
 const welcome = document.querySelector('#welcome')
 const welcomeForm = welcome.querySelector('form');
 
+
 async function initCall() {
     welcome.hidden = true;
     call.hidden = false;
     await getMedia();
     // RTC 연결
-    makeConnection();
+    return await makeConnection();
 }
-
-async function handleWelcomeSubmit (e) {
-    e.preventDefault();
-    const input = welcomeForm.querySelector('input');
-    await initCall()
-    socket.emit("join_room",input.value);
-    roomName = input.value;
-    input.value = '';
-}
-welcomeForm.addEventListener('submit', handleWelcomeSubmit);
-
-
-
-// socket code
-// peer A 브라우저에서 실행(먼저들어온 유저) 이미 들어와있는 유저에게만 실행됨
-socket.on('welcome', async ()=> {
-    console.log("누군가 입장했습니다.")
-    // offer는 초대장(접속코드)와 같음
-    const offer = await myPeerConnection.createOffer();
-    myPeerConnection.setLocalDescription(offer)
-    socket.emit("offer", offer, roomName)
-})
-
-// peer B 브라우저에서 실행(나중에 들어온 유저)
-socket.on('offer', async (offer)=> {
-    // console.log(offer)
-    myPeerConnection.setRemoteDescription(offer)
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit('answer', answer, roomName);
-})
-
-// peer A 브라우저에서 실행(먼저들어온 유저)
-socket.on('answer', answer => {
-    myPeerConnection.setRemoteDescription(answer)
-})
-
-socket.on('ice', ice => {
-    myPeerConnection.addIceCandidate(ice);
-})
 
 // RTC code
 // 연결을 만드는 함수
@@ -174,35 +163,193 @@ function makeConnection() {
             }
         ]
     });
-    myPeerConnection.addEventListener('icecandidate', handleIce)
-    myPeerConnection.addEventListener('addstream', handleAddStream)
+    users.set(mySocketID, {conn : myPeerConnection, nickname : ''});
+    myPeerConnection.addEventListener('icecandidate', handleIce);
+    myPeerConnection.addEventListener('addstream', handleAddStream);
+    myPeerConnection.addEventListener('connectionstatechange', event => {
+        if (myPeerConnection.connectionState === 'connected') {
+            // Peers connected!
+            console.log("연결되었습니다!")
+        };
+    });
     
     // addStream === addTrack 같은 함수
-    // myStream.getAudioTracks()[0].forEach(track=>myPeerConnection.addTrack(track, myStream))
-    
-    // 음성밖에 없기 때문에 반복문을 쓸 필요가 없음
-    myPeerConnection.addTrack(myStream.getAudioTracks()[0], myStream)
+    // 내 스트림에 있는 트랙들을 불러와서 피어 커넥션에 스트림을 추가한다.
+    if( myStream !== undefined ) {
+        myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream))
+
+        // myStream.getTracks().forEach(track => {
+        //     const myAudio = new Audio();
+        //     myAudio.srcObject = track
+        // })
+    }
+    return myPeerConnection;
+}
+
+async function otherUserConnection(user, mySocketID) {
+    const newConnection = new RTCPeerConnection({
+        iceServers : [
+            {
+                urls : [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302"
+                ]
+            }
+        ]
+    });
+    users.set(user.id, {conn: newConnection, nickname : user.nickname});
+    newConnection.addEventListener('icecandidate', (e)=> {
+        // 새로운 유저가 입장해서 발생되는 icecandidate이벤트
+        if (e.candidate) {
+            console.log('새로운 유저가 입장해서 발생되는 icecandidate이벤트 발생!')
+            socket.emit('otherUsersIce', {
+                mySocketID : mySocketID,
+                id : user.id,
+                candidate : e.candidate
+            })
+        }
+    })
+    newConnection.addEventListener('connectionstatechange' , (e)=> {
+        // console.log(e.connectionState)
+        if (newConnection.connectionState === "connected") {
+            console.log(`${user.id}와 연결되었습니다!`)
+        }
+    })
+    newConnection.ontrack = (e)=> {
+        console.log("데이터가 들어왔나요?")
+        console.log(e.streams)
+        const container = document.createElement('div');
+        container.classList.add('userStream');
+        container.classList.add(user.id);
+        const nickname = document.createElement('span');
+        nickname.textContent = user.nickname;
+        const video = document.createElement('video');
+        video.autoplay = true;
+        video.playsInline = true;
+        video.width = '200';
+        video.height = '200';
+        container.appendChild(nickname)
+        container.appendChild(video)
+        document.querySelector('#call').appendChild(container)
+        video.srcObject = e.streams[0];
+    }
+    const offer = await newConnection.createOffer({
+        offerToReceiveAudio : true,
+        offerToReceiveVideo : false
+    })
+    await newConnection.setLocalDescription(offer);
+    socket.emit('otherUserOffer', {
+        mySocketID : mySocketID,
+        id : user.id,
+        offer : offer
+    })
+    console.log(`${user.id}와 연결을 위해 RTC커넥션을 수립후 offer를 보냈습니다.`)
 }
 
 function handleIce(data) {
-    console.log(data)
-    console.log(myStream.getAudioTracks()[0])
+
     socket.emit('ice', data.candidate, roomName)
-    console.log("got icecandidate");
+    console.log(`iccCandidate를 (${data.candidate}) 서버(${roomName})에 보냈습니다.`);
 }
 
 function handleAddStream(data) {
     // 원본코드 {
-    const peerFace = document.querySelector('#peerFace');
+    // const peerFace = document.querySelector('#peerFace');
     // peerFace.srcObject = data.stream;
     // }
+    console.log("어떤 데이터가 들어있나요?")
+    console.log(data.stream)
     // const peerFace = document.createElement('video');
     // peerFace.autoplay = true;
     // peerFace.playsInline = true;
     // peerFace.style.width = '400px'
     // peerFace.style.height = '400px'
     // peerFace.srcObject = data.stream;
-    document.querySelector('#call').appendChild(peerFace)
-    console.log('got an event from my peer');
-    console.log(data)
+    // document.querySelector('#call').appendChild(peerFace)
 }
+
+// 내가 처음 방에 입장했을 때 발생됨
+
+async function handleWelcomeSubmit (e) {
+    e.preventDefault();
+    const input = welcomeForm.querySelector('input');
+    roomName = 'bigchoi'
+    await initCall();
+    // 내 닉네임 설정
+    users.get(mySocketID).nickname = input.value;
+    document.querySelector('.nickname').textContent = input.value;
+    console.log(users.get(mySocketID))
+    myNickname = input.value;
+    // 
+    const offer = await myPeerConnection.createOffer()
+    await myPeerConnection.setLocalDescription(offer)
+    const mydata = {
+        sdp : myPeerConnection.localDescription,
+        nickname : myNickname
+    }
+    socket.emit("join_room", mydata);
+    input.value = '';
+
+}
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+
+// socket code
+// 나와 서버의 연결에서 발생되는 answer이벤트
+
+// peer A 브라우저에서 실행(먼저들어온 유저) 이미 들어와있는 유저에게만 실행됨
+socket.on('welcome', async ()=> {
+    console.log("누군가 입장했습니다.")
+})
+
+socket.on('payload', (answer)=> {
+    console.log('payload 이벤트 발생')
+    // console.log(answer)
+    myPeerConnection.setRemoteDescription(answer).catch(e=>console.log(e))
+})
+
+socket.on('returnIce', async (ice) => {
+    console.log("ice를 받았습니다.")
+    await myPeerConnection.addIceCandidate(ice);
+    // console.log(myPeerConnection.iceConnectionState)
+})
+
+socket.on('newClient',  async (newClient)=> {
+    console.log("새로운 유저가 입장했습니다")
+    // 새로운 유저가 들어오면 RTC커넥션을 생성한 뒤에 offer를 보낸다.
+    otherUserConnection(newClient, mySocketID);
+})
+
+socket.on('newClientAnswer', async (data)=> {
+    // 새로운 클라이언트의 answer를 받았습니다.
+    console.log('새로운 클라이언트의 answer를 받았습니다.')
+    users.get(data.id).conn.setRemoteDescription(data.answer);
+})
+
+socket.on('newClientIce', (data)=> {
+    console.log("새로운 클라이언트의 RTC커넥션에서 ice 이벤트가 발생했습니다!");
+    users.get(data.id).conn.addIceCandidate(data.candidate);
+})
+
+
+socket.on('mySocketID', (id)=> {
+    mySocketID = id;
+})
+
+
+socket.on('otherUsers', (otherUsers)=> {
+    console.log("내가 아닌 다른 유저의 데이터를 받았습니다")
+    console.log(otherUsers)
+    otherUsers.map(user => otherUserConnection(user, mySocketID))
+})
+
+socket.on('outuser', (id)=> {
+    if (users.has(id)) {
+        users.get(id).conn.close();
+        document.querySelector(`.${id}`).remove()
+        users.delete(id);
+    }
+})
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import express from "express";
 // import  {Socket}  from "socket.io";
 // admin ui를 사용할 때
 import  Socket  from "socket.io";
+import wrtc from 'wrtc'
+
 
 let port = 3001;
 
@@ -21,20 +23,195 @@ app.get('/*', (req,res)=> res.redirect('/'))
 const server = http.createServer(app);
 const io = Socket(server);
 
+let roomName = "bigchoi"
+// 서버와 실제로 P2P로 주고받을 RTC objects
+const users = new Map();
+// const 
+// 다른 유저가 접속하면 클라에서 다른 유저의 갯수만큼 RTC커넥션을 만들겠지.
+// 그럼 그만큼 offer가 생길거고, 그렇다는건 하나의 클라이언트마다 모든 유저 - 자기자신만큼의 RTC커넥션을 생성한다는 것이고
+// 나중에 연결을 끊거나 하기 위해서는
+// 내 id마다 (모든 유저 - 자기자신만큼의 RTC커넥션)을 value로 가지고 있어야된다는 말
+// 때문에 실제 서버와 1:1로 접속되어 있는 object가 아닌 다른 유저들과의 커넥션을 관리할 object가 하나 더 필요하다는 얘기
+const connectionsExceptMe = new Map();
+
+const iceConfig = {
+    iceServers : [
+        {
+            urls : [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302"
+            ]
+        }
+    ]
+}
+
 io.on("connection", socket=> {
-    socket.on('join_room', (roomName)=> {
-        socket.join(roomName);
-        socket.to(roomName).emit("welcome")
+    socket.emit('mySocketID', socket.id);
+    socket.join('bigchoi');
+    socket.on('join_room', async (data)=> {
+        try {
+            // 새로운 유저가 들어오면 커넥션을 연결하고, 
+            const newConnection = new wrtc.RTCPeerConnection(iceConfig);
+            // user map에 새로 생성한 커넥션을 등록한다.
+            users.set(socket.id, {conn : newConnection, nickname : data.nickname});
+            // 온트랙 이벤트 등록
+            newConnection.ontrack = async (e) => {
+                users.get(socket.id).stream = e.streams[0]
+                // await newConnection.addTrack()
+            }
+            // 목적지 설정?
+            const desc = new wrtc.RTCSessionDescription(data.sdp);
+            // 받은 offer를 setRemoteDescription에 등록한다.(초대장이니까..)
+            await newConnection.setRemoteDescription(desc);
+            // 그 다음에 해당 커넥션에 answer를 생성 후에
+            const answer = await newConnection.createAnswer({
+                offerToReceiveAudio: true,
+                offerToReceiveVideo: true,
+            });
+            // 연결된 커넥션에 setLocalDescription(answer) 한다.
+            await newConnection.setLocalDescription(answer);
+
+            // console.log(users.get(socket.id))
+            newConnection.addEventListener('connectionstatechange' , (e)=> {
+                // console.log(e.connectionState)
+                if (newConnection.connectionState === "connected") {
+                    console.log("연결되었습니다!")
+                }
+            })
+            newConnection.addEventListener('icecandidate', (e)=> {
+                // console.log("icecandidate이벤트가 발생되었습니다.")
+                if (e.candidate) {
+                    socket.emit('returnIce', e.candidate)
+                }
+            })
+
+            socket.emit('payload', answer);
+
+            // socket.to(roomName).emit("welcome", nickname)
+            // 내가 아닌 다른 유저들 배열
+            const otherUsers = [];
+            for(const [key, value] of users ) {
+                console.log(socket.id, key)
+                if (socket.id !== key) {
+                    let otherUser = {
+                        id : key,
+                        nickname : value.nickname
+                    }
+                    otherUsers.push(otherUser);
+                }
+            }
+            
+            if (otherUsers.length > 0) {
+                console.log('다른 유저들',otherUsers.length)
+                // 내 자신에게 내가 아닌 다른 유저의 리스트를 보내준다.
+                socket.emit('otherUsers', otherUsers);
+            }
+            try {
+                // 내가 아닌 다른 사람들에게 내가 들어왔다고 알려야함.(이미 들어와있는 유저들에게)
+                socket.broadcast.to('bigchoi').emit('newClient', {
+                    id : socket.id,
+                    nickname : data.nickname
+                });
+            } catch (error) {
+                // console.log("왜 안 보내지니")
+            }
+        } catch (error) {
+            console.log(error)
+        }
     })
-    socket.on('offer', (offer, roomName) => {
-        console.log("offer(초대장)을 받았습니다.")
-        socket.to(roomName).emit('offer', offer);
+
+    // 클라이언트와 1:1 연결에서 쓰이는 ice 이벤트
+    socket.on('ice', async (ice, roomName)=> {
+        try {
+            if (users.has(socket.id)) {
+                await users.get(socket.id).conn.addIceCandidate(ice)
+                console.log("iceCandidate를 add했습니다.")
+            }
+        } catch (error) {
+            // console.log(error)
+        }
     })
-    socket.on('answer', (answer, roomName)=> {
-        socket.to(roomName).emit('answer', answer);
+
+
+
+    // 새로운 유저가 들어오면 아직 새로들어온 유저와 연결이 되어있지 않으니까
+    // 다시 연결을 수립해야된다.
+    socket.on('otherUserOffer', async (data)=> {
+        // return console.log(data)
+        // 새로운 offer(내가 아닌 다른 유저)를 받으면 새로운 커넥션을 만들고
+        const newConnection = new wrtc.RTCPeerConnection(iceConfig);
+        users.get(data.mySocketID).stream.getTracks().forEach(track => {
+            newConnection.addTrack(track, users.get(data.mySocketID).stream)
+        })
+        // 중복체크를 해야된다.
+        if (connectionsExceptMe.has(data.mySocketID)) {
+            // 만약에 있으면
+            const connections = connectionsExceptMe.get(data.mySocketID).connections;
+            console.log(connections)
+            connections.map(obj => obj.id !== data.id ? connections.push({id : data.id, conn : newConnection}) : null)
+        } else {
+            // 없으면 conntions 배열에 초기값을 추가한다.
+            connectionsExceptMe.set(data.mySocketID, {connections : [{
+                id : data.id,
+                conn : newConnection
+            }]})
+        }
+        newConnection.addEventListener('icecandidate', (e)=> {
+            // 새로운 유저가 입장해서 발생되는 icecandidate이벤트
+            if (e.candidate) {
+                console.log('새로운 유저가 입장해서 발생되는 icecandidate이벤트 발생!')
+                io.to(data.mySocketID).emit('newClientIce', {
+                    mySocketID : data.mySocketID,
+                    id : data.id,
+                    candidate : e.candidate
+                })
+            }
+        })
+        newConnection.addEventListener('connectionstatechange' , (e)=> {
+            // console.log(e.connectionState)
+            if (newConnection.connectionState === "connected") {
+                console.log(`${data.id}와 연결되었습니다!`)
+            }
+        })
+        // return console.log(connectionsExceptMe.get(data.mySocketID).connections)
+        // const desc = await new wrtc.RTCSessionDescription(data.offer);
+        // return console.log(data.offer);
+        await newConnection.setRemoteDescription(data.offer);
+        const answer = await newConnection.createAnswer({
+            offerToReceiveAudio: false,
+            offerToReceiveVideo: false,
+        })
+        await newConnection.setLocalDescription(answer);
+        console.log(data.mySocketID)
+        io.to(data.mySocketID).emit('newClientAnswer', {
+            mySocketID : data.mySocketID,
+            id : data.id,
+            answer : answer
+        })
     })
-    socket.on('ice', (ice, roomName)=> {
-        socket.to(roomName).emit('ice', ice)
+
+    socket.on('otherUsersIce', (data)=> {
+        const connections = connectionsExceptMe.get(data.mySocketID).connections;
+        connections.map(obj => obj.id === data.id ? obj.conn.addIceCandidate(data.candidate) : null)
+    })
+
+    socket.on('disconnecting', ()=> {
+        // socket.id 연결 해제 및 map에서 삭제
+    })
+    socket.on('disconnect', ()=> {
+        try {
+            if (users.has(socket.id)) {
+                users.get(socket.id).conn.close();
+                io.to(roomName).emit('outuser', socket.id);
+                users.delete(socket.id)
+            }
+        } catch (error) {
+            // console.log(error)
+        }
+        // socket.id가 속해있는 방에 나간 소식을 전달
     })
 })
 
